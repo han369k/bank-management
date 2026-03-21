1567ac613704756fbafcd6b6baaa6d14eecb39c4
Shih477 <q10301761968@gmail.com>
2026-03-21 15:40:10 +0800

    [feat] 新增BaseDao用來簡化Dao實作

---
+++ src/main/java/com/ispan/bankmanagement/stock/dao/BaseDao.java
@@ -0,0 +1,56 @@
+package com.ispan.bankmanagement.stock.dao;
+
+import com.ispan.bankmanagement.util.ConnUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseDao {
+
+    @FunctionalInterface
+    public interface RowMapper<T> {
+        T mapRow(ResultSet rs) throws SQLException;
+    }
+
+    //<T>宣告使用泛型 回傳List<T>
+    //Object... params 能夠接受不限定數量的參數 對應SQL語句的「?」(包括0個 也就是只填sql跟rowMapper兩個參數)
+    protected <T> List<T> Query(String sql, RowMapper<T> rowMapper, Object... params) {
+        List<T> list = new ArrayList<>();
+        try (Connection conn = ConnUtil.getConn();
+             PreparedStatement ps = conn.prepareStatement(sql);
+        ) {
+            for (int i = 0; i < params.length; i++) {
+                ps.setObject(i + 1, params[i]);
+            }
+            try (ResultSet rs = ps.executeQuery();) {
+                while (rs.next()) {
+                    list.add(rowMapper.mapRow(rs));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    //增刪改都通用 因為他們都是用PreparedStatement.executeUpdate()操作SQL
+    //回傳int 表示異動了多少筆資料
+    protected int Update(String sql, Object... params) {
+
+        try (Connection conn = ConnUtil.getConn();
+             PreparedStatement ps = conn.prepareStatement(sql);) {
+            if (params != null) {
+                for (int i = 0; i < params.length; i++) {
+                    ps.setObject(i + 1, params[i]);
+                }
+            }
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("資料庫更新失敗: " + e.getMessage(), e);
+        }
+    }
+}
--- src/main/java/com/ispan/bankmanagement/stock/dao/StockInfoDao.java
+++ src/main/java/com/ispan/bankmanagement/stock/dao/StockInfoDao.java
@@ -1,67 +1,41 @@
 package com.ispan.bankmanagement.stock.dao;
 
 import com.ispan.bankmanagement.stock.entity.StockInfoEntity;
-import com.ispan.bankmanagement.util.ConnUtil;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class StockInfoDao {
+public class StockInfoDao extends BaseDao {
+
+    //lambda語法
+    private final RowMapper<StockInfoEntity> rowMapper = rs -> {
+        StockInfoEntity entity = new StockInfoEntity();
+        entity.setStockName(rs.getString("stock_name"));
+        entity.setStockId(rs.getInt("stock_id"));
+        return entity;
+    };
 
     public List<StockInfoEntity> GetAll() {//查詢全部
-        String sql = "select stock_id, stock_name from stock_info";
-        List<StockInfoEntity> list = new ArrayList<>();
-
-        try (Connection conn = ConnUtil.getConn();
-             PreparedStatement ps = conn.prepareStatement(sql);
-             ResultSet rs = ps.executeQuery();
-        ) {
-            while (rs.next()) {
-                StockInfoEntity stockInfo = new StockInfoEntity();
-                stockInfo.setStockId(rs.getInt("stock_id"));
-                stockInfo.setStockName(rs.getString("stock_name"));
-                list.add(stockInfo);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return list;
+        String getAll = "select stock_id, stock_name from stock_info";
+        return Query(getAll, rowMapper);
     }
 
     public StockInfoEntity GetById(int id) {//查詢單筆
-        String sql = "select stock_id, stock_name from stock_info where stock_id = ?";
-        StockInfoEntity stockInfo = new StockInfoEntity();
-        try (Connection conn = ConnUtil.getConn();
-             PreparedStatement ps = conn.prepareStatement(sql);
-        ) {
-            ps.setInt(1, id);
-            try (ResultSet rs = ps.executeQuery();) {
-                if (rs.next()) {
-                    stockInfo.setStockId(rs.getInt("stock_id"));
-                    stockInfo.setStockName(rs.getString("stock_name"));
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return stockInfo;
+        String getById = "select stock_id, stock_name from stock_info where stock_id = ?";
+        // 把查到的 List 丟進流裡，抓第一個，抓不到就回傳 null
+        return Query(getById, rowMapper, id).stream().findFirst().orElse(null);
     }
 
     public void InsertStockInfo(StockInfoEntity stockInfo) {//新增單筆
-        String sql = "insert into stock_info (stock_id, stock_name) values (?, ?)";
-        try (Connection conn = ConnUtil.getConn();
-             PreparedStatement ps = conn.prepareStatement(sql);
-        ) {
-            ps.setInt(1, stockInfo.getStockId());
-            ps.setString(2, stockInfo.getStockName());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        String insert = "insert into stock_info (stock_id, stock_name) values (?, ?)";
+        Update(insert, stockInfo.getStockId(), stockInfo.getStockName());
+    }
 
+    public void DeleteById(int id) {//依據id刪除
+        String delete = "delete from stock_info where stock_id = ?";
+        Update(delete, id);
+    }
+
+    public void UpdateStockInfo(StockInfoEntity stockInfo) {//依據id更新資料
+        String update = "update stock_info set stock_name = ? where stock_id = ?";
+        Update(update, stockInfo.getStockName(), stockInfo.getStockId());
     }
 }
