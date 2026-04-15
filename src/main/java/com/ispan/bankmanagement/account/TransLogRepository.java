5e0fbff82f7fcf1f8f58327648bc1ddddbedc915
Hank <azsx1539004222@gmail.com>
2026-04-15 19:26:36 +0800

    [feat]add account translog repository

---
+++ src/main/java/com/ispan/bankmanagement/account/TransLogRepository.java
@@ -0,0 +1,7 @@
+package com.ispan.bankmanagement.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransLogRepository extends JpaRepository<TransLog,Integer>{
+
+}
