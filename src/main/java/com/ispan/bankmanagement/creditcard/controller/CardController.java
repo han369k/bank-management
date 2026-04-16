e7fa52ef686a55c48c6368b29aad3126e28f1d8c
john <lucaswang193@gmail.com>
2026-04-17 00:34:54 +0800

    Refactor CardController to RestController and update related views

--- src/main/java/com/ispan/bankmanagement/creditcard/controller/CardController.java
+++ src/main/java/com/ispan/bankmanagement/creditcard/controller/CardController.java
@@ -1,70 +1,68 @@
 package com.ispan.bankmanagement.creditcard.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ispan.bankmanagement.creditcard.entity.CardApplication;
+import com.ispan.bankmanagement.creditcard.entity.CardType;
+import com.ispan.bankmanagement.creditcard.entity.CreditCard;
 import com.ispan.bankmanagement.creditcard.service.CardAppService;
 import com.ispan.bankmanagement.creditcard.service.CardTypeService;
 import com.ispan.bankmanagement.creditcard.service.CreditCardService;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RestController
 @RequestMapping("/card")
 @RequiredArgsConstructor
 public class CardController {
 
-	private CardTypeService cardTypeService;
-	private CreditCardService creditCardService;
-	private CardAppService cardAppService;
+	private final CardTypeService cardTypeService;
+	private final CreditCardService creditCardService;
+	private final CardAppService cardAppService;
 	
 	//卡片列表
-	//http://localhost:8080/card/types.controller
+	
+	//http://localhost:8080/card/types
 	@GetMapping("/types")
-	public String getAllCardType(Model model) {
-		model.addAttribute("cardTypes",cardTypeService);
-		return "cardType-list";
+	public List<CardType> getAllCardType() {
+		return cardTypeService.findAll();
 	}
 	//卡片詳細
+	//http://localhost:8080/card/types/1
 	@GetMapping("/types/{id}")
-	public String getCardTypeDetail(@PathVariable Integer id,Model model) {
-		model.addAttribute("cardType",cardTypeService.findById(id));
-		return "cardType-detail";
+	public CardType getCardTypeDetail(@PathVariable Integer id) {
+		return cardTypeService.findById(id);
+		
 	}
 	// 申請信用卡
+	//http://localhost:8080/card/apply 此不能透過瀏覽器
 	@PostMapping("/apply")
-	public String applyCard(@ModelAttribute CardApplication application) {
+	public ResponseEntity<String> applyCard(@RequestBody CardApplication application) {
 		cardAppService.save(application);
-		return "apply";
+		return ResponseEntity.ok("Application submitted.");
 	}
 	//我的申請
+	//http://localhost:8080/card/my-applications
 	@GetMapping("/my-applications")
-    public String getMyApplications(Model model) {
-        model.addAttribute("applications", cardAppService.findAll());
-        return "my-applications";
+    public List<CardApplication> getMyApplications() {
+         return cardAppService.findAll();
     }
 	// 我的卡片
+	//http://localhost:8080/card/my-cards
 	@GetMapping("/my-cards")
-    public String getMyCards(Model model) {
-        model.addAttribute("cards", creditCardService.findAll());
-        return "my-cards";
+    public List<CreditCard> getMyCards() {
+        return creditCardService.findAll();
     }
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 }
---
+++ src/test/java/com/ispan/bankmanagement/creditcard/controller/CardControllerTest.java
@@ -0,0 +1,46 @@
+package com.ispan.bankmanagement.creditcard.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ispan.bankmanagement.creditcard.service.CardTypeService;
+import com.ispan.bankmanagement.creditcard.service.CreditCardService;
+@SpringBootTest
+class CardControllerTest {
+
+//	@Autowired
+//	private CreditCardService cardService;
+//	@Autowired
+//	private CardTypeService cardTypeService;
+//	
+//	
+//	@Test
+//	void testGetAllCardType() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testGetCardTypeDetail() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testApplyCard() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testGetMyApplications() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testGetMyCards() {
+//		fail("Not yet implemented");
+//	}
+
+}
