cc001f1968160566ad169e3eb0a6b0df534dac56
Hank <azsx1539004222@gmail.com>
2026-04-19 16:58:19 +0800

    [feat] 配合Dto更新import

--- src/main/java/com/ispan/bankmanagement/account/AccountRepository.java
+++ src/main/java/com/ispan/bankmanagement/account/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.ispan.bankmanagement.account;
 
+import com.ispan.bankmanagement.account.enums.AccountStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
